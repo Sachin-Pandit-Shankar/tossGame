4a49dc3f62fa4e2a9045f518739cdc4cefa9b326
Sachin pandit <127498624+Sachin-Pandit-Shankar@users.noreply.github.com>
2023-07-29 13:44:48 +0530

    Update index.js

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -1,73 +1,55 @@
 // Write your code here
-// Write your code here
-
-import './index.css'
 import {Component} from 'react'
+import './index.css'
 
 class CoinToss extends Component {
   state = {
-    isHead: true,
-    totalScore: 0,
-    totalHeads: 0,
-    totalTails: 0,
+    isHeads: 'true',
+    heads: 0,
+    Tails: 0,
+    TotalScore: 0,
   }
 
-  onClickTossCoin = () => {
+  onClickChangeStatus = () => {
     const getNum = Math.floor(Math.random() * 2)
     if (getNum === 0) {
       this.setState(prevState => ({
-        isHead: true,
-        totalScore: prevState.totalScore + 1,
-        totalHeads: prevState.totalHeads + 1,
+        isHeads: true,
+        heads: prevState.heads + 1,
+        TotalScore: prevState.TotalScore + 1,
       }))
     } else if (getNum === 1) {
       this.setState(prevState => ({
-        isHead: false,
-        totalScore: prevState.totalScore + 1,
-        totalTails: prevState.totalTails + 1,
+        isHeads: false,
+        Tails: prevState.Tails + 1,
+        TotalScore: prevState.TotalScore + 1,
       }))
     }
   }
 
   render() {
-    const {isHead, totalScore, totalHeads, totalTails} = this.state
-    const srcImage = isHead
+    const {isHeads, heads, Tails, TotalScore} = this.state
+    const getTossImage = isHeads
       ? 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
       : 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
 
     return (
-      <div className="app-container">
-        <div className="coin-toss-container">
-          <div className="header-container">
-            <h1 className="game-heading">Coin Toss Game</h1>
-            <p className="game-sub-heading">Heads (or) Tails </p>
-          </div>
-          <div className="image-container">
-            <img
-              src={srcImage}
-              alt="toss result"
-              className="heads-tails-image"
-            />
-          </div>
-          <div className="toss-coin-button-container">
-            <button
-              className="toss-coin-button"
-              type="button"
-              onClick={this.onClickTossCoin}
-            >
-              Toss Coin
-            </button>
-          </div>
-          <div className="scores-container">
-            <div className="total-score-container">
-              <p className="score-status">Total: {totalScore}</p>
-            </div>
-            <div className="total-score-container">
-              <p className="score-status">Heads: {totalHeads}</p>
-            </div>
-            <div className="total-score-container">
-              <p className="score-status">Tails: {totalTails}</p>
-            </div>
+      <div className="bg-container">
+        <div className="white-container">
+          <h1 className="main-heading">Coin Toss Game</h1>
+          <p className="top-text">Heads (or) Tails</p>
+          <img src={getTossImage} alt="toss-result" className="coin-image" />
+          <button
+            type="button"
+            onClick={this.onClickChangeStatus}
+            className="coin-btn"
+          >
+            Toss Coin
+          </button>
+          <div className="score-card">
+            <p className="total-score">{`Total: ${TotalScore}`}</p>
+            <p className="total-score">{`Heads: ${heads}`}</p>
+            <p className="total-score">{`Tails: ${Tails}`}</p>
           </div>
         </div>
       </div>
